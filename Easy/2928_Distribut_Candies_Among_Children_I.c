2133b240039e26cd79e56543a77ef3f1e9907ef1
Anas El mendili <anel-men@e3r6p6.1337.ma>
2024-11-02 15:52:50 +0100

    2928

---
+++ Easy/2928_Distribut_Candies_Among_Children_I.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int distributCandies(int n, int limit)
+{
+    int a = 0, b, c, total_ways = 0;
+    while(a <= limit && a <= n)
+    {
+        int remaining = n - a;
+        b = 0;
+        while(b <= limit && b <= n)
+        {
+            c = remaining - b;
+            if (c >= 0 && c <= limit)
+            {
+                total_ways++;
+            }
+            b++;
+        }
+        a++;
+    }
+    return total_ways;
+}
+
+//int main()
+//{
+//    int n1 = 5;
+//    int limit1 = 2;
+//    printf("Number of ways to distribut %d candies with limit %d: %d\n", n1, limit1, distributCandies(n1, limit1));
+//
+ //   int n2 = 3;
+//    int limit2 = 3;
+ //   printf("Number of ways to distribut %d candies with limit %d: %d\n", n1, limit2, distributCandies(n2, limit2));
+//
+ //   return (0);
+//
+//}
